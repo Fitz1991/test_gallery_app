689d829fdb56fe63b5a59ddb4723c09f6ab7a5eb
Дмитрий <dpotryasilov@gmail.com>
2020-12-22 17:07:56 +0300

    delete navigation bloc

--- lib/gallery_page.dart
+++ lib/gallery_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:test_gallery_app/domain/gallery/gallery_bloc.dart';
+import 'package:test_gallery_app/generated/assets.dart';
 import 'package:test_gallery_app/model/gallery_image.dart';
 
 class GalleryPage extends StatelessWidget {
@@ -35,20 +36,39 @@ class GalleryPage extends StatelessWidget {
               itemCount: state.images.length,
               itemBuilder: (context, index) {
                 return Container(
+                  padding: EdgeInsets.all(10),
                   height: 300,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       Expanded(
-                        child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: EdgeInsets.all(5.0),
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                              child: Image.network(state.images[index].url, fit: BoxFit.cover,)),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/full_screen/${state.images[index].id}');
+                            // BlocProvider.of<NavigationBloc>(context).add(NavigateTo(NavItem.full_screen));
+                          },
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.all(5.0),
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                                child: Image.network(state.images[index].url, fit: BoxFit.cover,)),
+                          ),
                         ),
                       ),
-                      SvgPicture.asset('assets/heart-solid.svg')
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(20),
+                          onTap: () {
+
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SvgPicture.asset(Assets.assetsLike, color: Colors.black12, width: 30, height: 30,),
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_gallery_app/domain/gallery/gallery_bloc.dart';
+import 'package:test_gallery_app/full_screen.dart';
 
 import 'gallery_page.dart';
 
@@ -13,14 +14,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      onGenerateRoute: (routeSettings){
+        var path = routeSettings.name.split('/');
+
+        if (path[1] == "full_screen") {
+          return new MaterialPageRoute(
+            builder: (context) => FullScreen(),
+            settings: routeSettings,
+          );
+        } else{
+          return new MaterialPageRoute(
+            builder: (context) => MyHomePage(),
+            settings: routeSettings,
+          );
+        }
+      },
       title: 'Test gallery app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: BlocProvider<GalleryBloc>(create: (context) =>
-      GalleryBloc()
-        ..add(LoadImages(page: 1)), child: MyHomePage(title: 'Test gallery app')),
+      home:
+      MultiBlocProvider(
+        providers: [
+          BlocProvider<GalleryBloc>(create: (context) =>
+          GalleryBloc()
+            ..add(LoadImages(page: 1))),
+        ],
+        child: MyHomePage(title: 'Test gallery app'),
+      )
+      ,
     );
   }
 }
